cbf55e3d3a77f47854956939b209171d111893fd
Selyanyn <56536978+Selyanyn@users.noreply.github.com>
2022-03-22 22:43:49 +0500

    Refactored CardC

--- app/src/main/java/com/example/a2_cards_deck/CardC.java
+++ app/src/main/java/com/example/a2_cards_deck/CardC.java
@@ -1,13 +1,20 @@
 package com.example.a2_cards_deck;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CardC {
     private String suit;
     private int rank;
 
-    static int spadesMaxRank = 1;
-    static int diamondsMaxRank = 1;
-    static int clubsMaxRank = 1;
-    static int heartsMaxRank = 1;
+    static Map<String, Integer> maxRank;
+    static {
+        maxRank = new HashMap<String, Integer>();
+        maxRank.put("spades", 1);
+        maxRank.put("diamonds", 1);
+        maxRank.put("clubs", 1);
+        maxRank.put("hearts", 1);
+    }
 
     public CardC(String suit, int rank) throws Exception {
         if (!suit.equals("spades") &&
@@ -30,18 +37,8 @@ public class CardC {
             throw new Exception("Такой масти нет!");
         }
         this.suit = suit;
-        if (suit.equals("spades")) {
-            rank = spadesMaxRank++;
-        }
-        if (suit.equals("diamonds")) {
-            rank = spadesMaxRank++;
-        }
-        if (suit.equals("clubs")) {
-            rank = spadesMaxRank++;
-        }
-        if (suit.equals("hearts")) {
-            rank = spadesMaxRank++;
-        }
+        maxRank.put(suit, maxRank.get(suit) + 1);
+        this.rank = maxRank.get(suit);
     }
 
     public String getSuit() {
